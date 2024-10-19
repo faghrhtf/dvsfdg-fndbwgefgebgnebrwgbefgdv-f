9d2baa329ecde60597b1f3571a14eadce13ab6ca
faghrhtf <faghrhtf@gmail.com>
2024-10-19 17:32:18 +0300

    Add files via upload

---
+++ change.bat
@@ -0,0 +1,73 @@
+@echo off
+:: BatchGotAdmin
+color 0A
+cls
+
+:CHECKADMIN
+REM --> Check for admin permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting Admin...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+set params= %*
+echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+pushd "%CD%"
+CD /D "%~dp0"
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+SETLOCAL ENABLEEXTENSIONS
+
+:RANDOMIZE
+echo CHANGING MAC... PLEASE WAIT
+FOR /F "tokens=1" %%a IN ('wmic nic get deviceid ^| findstr [0-9]') DO (
+    CALL :MAC
+    FOR %%b IN (0 00 000) DO (
+        REG QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a >NUL 2>NUL && REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\%%b%%a /v NetworkAddress /t REG_SZ /d !MAC! /f >NUL 2>NUL
+    )
+)
+echo.
+
+goto RESETNIC
+
+:RESETNIC
+FOR /F "tokens=2 delims=, skip=2" %%a IN ('"wmic nic get netconnectionid,netconnectionstatus /format:csv"') DO (
+    netsh interface set interface name="%%a" disable >NUL 2>NUL
+    netsh interface set interface name="%%a" enable >NUL 2>NUL
+)
+timeout /t 2 >NUL
+exit /B
+
+:MAC
+:: Generates semi-random value of a length according to the "if !COUNT!" line, minus one, and from the characters in the GEN variable
+SET COUNT=0
+SET GEN=ABCDEF0123456789
+SET GEN2=26AE
+SET MAC=
+:MACLOOP
+SET /a COUNT+=1
+SET RND=%random%
+SET /A RND=RND%%16
+SET RNDGEN=!GEN:~%RND%,1!
+SET /A RND2=RND%%4
+SET RNDGEN2=!GEN2:~%RND2%,1!
+IF "!COUNT!"  EQU "2" (SET MAC=!MAC!!RNDGEN2!) ELSE (SET MAC=!MAC!!RNDGEN!)
+IF !COUNT!  LEQ 11 GOTO MACLOOP
+GOTO :EOF
+echo DONE
+EXIT
